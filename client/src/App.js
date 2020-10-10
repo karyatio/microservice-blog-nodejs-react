da541394fedc4b156c3cb62ebdc03edaa3f4aff9
karyaTio <karya.tiosaputra@gmail.com>
2020-10-10 18:55:37 +0700

    Comment create and list

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
 import PostCreate from "./PostCreate";
+import PostList from "./PostList";
 
 export default () => {
   return (
     <div className="container">
       <h1>Create Post</h1>
       <PostCreate />
+
+      <hr />
+      <h1>List of posts</h1>
+      <PostList />
     </div>
   );
 };
